65e567b59622f0c6d065147393614d33601557fa
Zhangcjxinjie <zhangx5@spu.edu>
2023-02-18 19:44:07 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SpeechContestProcessManagement.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpeechContestProcessManagement.dir/main.cpp.o"
+  "CMakeFiles/SpeechContestProcessManagement.dir/main.cpp.o.d"
+  "SpeechContestProcessManagement"
+  "SpeechContestProcessManagement.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpeechContestProcessManagement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
